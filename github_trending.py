46603e19861bc7dd63ddd82696388bc7500c2751
conformist-mw <conformist.mw@gmail.com>
2016-10-29 23:11:55 +0300

    changes are applied

--- github_trending.py
+++ github_trending.py
@@ -1,32 +1,32 @@
 import requests
-import datetime as DT
+from datetime import date, timedelta
 
 
-def week():
-    today = DT.date.today()
-    return str(today - DT.timedelta(days=7))
+def week_ago():
+    today = date.today()
+    return str(today - timedelta(days=7))
 
 
 def get_trending_repositories(top_size):
     url = 'https://api.github.com/search/repositories'
-    params = {'q': 'created:>' + week(), 'sort': 'stars', 'order': 'desc'}
+    params = {'q': 'created:>' + week_ago(), 'sort': 'stars', 'order': 'desc'}
     response = requests.get(url, params=params)
     repos = response.json()['items'][:top_size]
     return [repo['full_name'] for repo in repos]
 
 
-def get_open_issues_amount(full_name):
-    url = 'https://api.github.com/repos/{}/issues'.format(full_name)
+def collect_open_issues_links(repo_full_name):
+    url = 'https://api.github.com/repos/{}/issues'.format(repo_full_name)
     response = requests.get(url).json()
     return ['\t' + link['url'] for link in response]
 
 
 if __name__ == '__main__':
-    repo_names = get_trending_repositories(20)
-    for name in repo_names:
+    trend_repos = get_trending_repositories(20)
+    for name in trend_repos:
         user, repo = name.split('/')
         print('User: {}, repo: {}'.format(user, repo))
-        issues = get_open_issues_amount(name)
+        issues = collect_open_issues_links(name)
         print('Issues amount: {}'.format(len(issues)))
         if len(issues) > 0:
             print('Links: ')
